127ce356bf9fa11863b38d1594f0655a33c5487f
Thiago Serafina <85206010+thiagoserafina@users.noreply.github.com>
2022-10-11 17:20:12 -0300

    Add files via upload

--- src/lib/OperacoesMatematicas.java
+++ src/lib/OperacoesMatematicas.java
@@ -1,36 +1,50 @@
 package lib;
 
 public class OperacoesMatematicas {
-	public float AplicaDesconto (float a, float b) {
-		return a - (a *(b / 100));
+	public static float AplicaDesconto(float a, float b) {
+		return a - (a * (b / 100));
 	}
-	
-	public float AplicaAcrecimo (float a, float b) {
-		return a + (a *(b / 100));
+
+	public static float AplicaAcrecimo(float a, float b) {
+		return a + (a * (b / 100));
 	}
-	
-	public float Amostragem (float a, float b) {
+
+	public static float Amostragem(float a, float b) {
 		return (a * b) / 100;
 	}
-	
-	public float Amostragem2 (float a, float b) {
+
+	public static float Amostragem2(float a, float b) {
 		return (a / b) * 100;
 	}
-	
-	public float DescontoAplicado (float a, float b) {
-		return ((a - b) / a ) * 100;
+
+	public static float DescontoAplicado(float a, float b) {
+		return ((a - b) / a) * 100;
 	}
-	
-	public float VariacaoDelta (float a, float b) {
-		return (b / a - 1 ) * 100;
+
+	public static float VariacaoDelta(float a, float b) {
+		return (b / a - 1) * 100;
 	}
-	
-	public float ValorOriginal (float a, float b) {
-		return ( a * 100 / ( 100 - b));
+
+	public static float ValorOriginal(float a, float b) {
+		return (a * 100 / (100 - b));
 	}
-	
-	public float RegraDeTres (float a, float b, float c) {
-		return (c * b) / a ;
+
+	public static float RegraDeTres(float a, float b, float r1) {
+		return (b * r1) / a;
 	}
-	
+
+	public static String GeradorDeSenhas(boolean a, boolean b, boolean c, boolean d, int tam) {
+		String lowCase = "abcdefghijklmnopqrstuvxyz";
+		String upCase = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+		String Numbers = "0123456789";
+		String SpecialChar = "£$&()*+[]@#^-_!?";
+		// int charCategories = 4;
+		int passLength = tam;
+
+
+
+
+		return "";
+	}
+
 }
